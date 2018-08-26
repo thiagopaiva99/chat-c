d17e1b315b9e4d2cc2eb44599484dfb5a4368ada
thiagopaiva99 <thiagopaiva99@gmail.com>
2018-08-26 16:51:24 -0300

    Adding colors from users that entry on the chat

--- client/client.c
+++ client/client.c
@@ -3,20 +3,58 @@
 #include <stdlib.h>
 #include <string.h>
 #include <netinet/in.h>
+#include <time.h>
+
+int getRandom(int lower, int upper, int count)
+{
+    int i;
+    int num;
+
+    for (i = 0; i < count; i++)
+    {
+        num = (rand() % (upper - lower + 1)) + lower;
+    }
+
+    return num;
+}
+
+char *getColor(int number)
+{
+    switch (number)
+    {
+    case 0:
+        return "\x1b[31m";
+    case 1:
+        return "\x1b[32m";
+    case 2:
+        return "\x1b[33m";
+    case 3:
+        return "\x1b[34m";
+    case 4:
+        return "\x1b[35m";
+    }
+
+    return "aa";
+}
 
 int main(void)
 {
-    char *message[1000];
+    char message[1000];
     struct sockaddr_in server;
     int fd;
-    char *aux[1500];
-    char *name[50];
+    char aux[1500];
+    char name[50];
+    int color;
 
     fd = socket(AF_INET, SOCK_STREAM, 0);
     server.sin_family = AF_INET;
     server.sin_port = htons(8096);
-    inet_pton(AF_INET, "127.0.0.1", &server.sin_addr);       //This binds the client to localhost
-    connect(fd, (struct sockaddr *)&server, sizeof(server)); //This connects the client to the server.
+    inet_pton(AF_INET, "127.0.0.1", &server.sin_addr);
+    connect(fd, (struct sockaddr *)&server, sizeof(server));
+
+    srand(time(0));
+
+    color = getRandom(0, 4, 1);
 
     printf("Say your name: ");
     fgets(name, 50, stdin);
@@ -28,7 +66,9 @@ int main(void)
         printf("Enter a message: ");
         fgets(message, 100, stdin);
 
-        strcpy(aux, name);
+        strcpy(aux, getColor(color));
+        strcat(aux, name);
+        strcat(aux, "\x1b[0m");
         strcat(aux, " say: ");
         strcat(aux, message);
 
